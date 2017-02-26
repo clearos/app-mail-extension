097dd01b5666257e7b574bc8b72fbec6fcde1462
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:55:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = '邮件扩展';
+$lang['mail_extension_app_description'] = '该邮件扩展扩展了用户邮件和别名属性的目录。';
+$lang['mail_extension_aliases'] = '别名';
+$lang['mail_extension_email'] = '电子邮件';
+$lang['mail_extension_forwarders'] = '代理';
+$lang['mail_extension_mail'] = '邮件';
+$lang['mail_extension_email_invalid'] = '电子邮箱地址不可用。';
+$lang['mail_extension_distribution_list'] = '分发列表';
+
+$lang['mail_extension_mail_alias_invalid'] = '邮件别名是无效的。';
+$lang['mail_extension_mail_forwarder_invalid'] = '邮件转发无效。';
+
