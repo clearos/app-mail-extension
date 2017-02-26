378edee53ec1069bb4a070c80980c25874a197fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:55:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension простира директорията с потребителското поща и псевдоним атрибути.';
+$lang['mail_extension_aliases'] = 'Псевдоними';
+$lang['mail_extension_email'] = 'Е-поща';
+$lang['mail_extension_forwarders'] = 'спедитори';
+$lang['mail_extension_mail'] = 'поща';
+$lang['mail_extension_email_invalid'] = 'Имейл адресът е невалиден.';
+$lang['mail_extension_distribution_list'] = 'Списък Разпределение';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail псевдоним е невалиден.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail спедитор е невалиден.';
+
