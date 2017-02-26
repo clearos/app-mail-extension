abccf15b823c66c6602ee8691a5f104a5f1ba923
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:56:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension udvider biblioteket med brugerens mail og alias attributter.';
+$lang['mail_extension_aliases'] = 'aliasser';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Udkørselsmaskine';
+$lang['mail_extension_mail'] = 'Post';
+$lang['mail_extension_email_invalid'] = 'Email adressen er ugyldig.';
+$lang['mail_extension_distribution_list'] = 'Omdelingsliste';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias er ugyldig.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail speditør er ugyldig.';
+
