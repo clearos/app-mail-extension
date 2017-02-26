99ffe8cbc978a34d890c5e94cb63378e0e9ab778
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:54:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Փոստ Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension երկարացնում է ցուցակում օգտվողին փոստի եւ alias հատկանիշներ:';
+$lang['mail_extension_aliases'] = 'aliases';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'փոստ';
+$lang['mail_extension_email_invalid'] = 'E-mail հասցեն անվավեր է:';
+$lang['mail_extension_distribution_list'] = 'բաշխման ցուցակը';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Փոստ Ծածկանուն է անվավեր:';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Փոստ առաքիչը է անվավեր:';
+
