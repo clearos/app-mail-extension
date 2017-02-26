0ab5a150055ad785ea6c02aad1d64764301016d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:56:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension proteže imenik sa user-pošte i pseudonim atributa.';
+$lang['mail_extension_aliases'] = 'nadimci';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'otpremnici';
+$lang['mail_extension_mail'] = 'pošta';
+$lang['mail_extension_email_invalid'] = 'Adresa e-pošte nije valjana.';
+$lang['mail_extension_distribution_list'] = 'popis za slanje';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias nije valjan.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail špediter je nevažeći.';
+
