85d5cea52f7c9bb9bff443e058b03880baa5daa0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:55:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension amawapatsa Directory ndi makhalidwe wosuta makalata ndi alias.';
+$lang['mail_extension_aliases'] = 'Aliases';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'E-mail adiresi ili udzakhala osazindikilika.';
+$lang['mail_extension_distribution_list'] = 'yogawa List';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias ndi opanda pake.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder ndi opanda pake.';
+
