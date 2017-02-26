f81761ce3bf4f75b5162705e86c62e9e485c4730
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:54:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension shtrihet në dosjen me postë përdoruesit dhe alias atributet.';
+$lang['mail_extension_aliases'] = 'emërtimet';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'postë';
+$lang['mail_extension_email_invalid'] = 'Adresa E-mail është i pavlefshëm.';
+$lang['mail_extension_distribution_list'] = 'Lista e shpërndarjes';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias është e pavlefshme.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder është e pavlefshme.';
+
