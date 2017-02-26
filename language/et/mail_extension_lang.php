74add3cd8f0350106c0c7609f36b8864f4a023d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:56:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail pikendamine';
+$lang['mail_extension_app_description'] = 'Mail pikendamine pikendab kataloog kasutaja mail ja alias atribuute.';
+$lang['mail_extension_aliases'] = 'Varjunimede';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'forvarderid';
+$lang['mail_extension_mail'] = 'post';
+$lang['mail_extension_email_invalid'] = 'Emaili aadress on vale.';
+$lang['mail_extension_distribution_list'] = 'postiloendina';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias on kehtetu.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail ekspediitor on kehtetu.';
+
