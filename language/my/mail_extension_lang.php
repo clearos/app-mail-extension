e234b79f753d990ced162f55423e6e76940cc8bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:55:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'မေးလ်တိုးချဲ့မှု';
+$lang['mail_extension_app_description'] = 'အဆိုပါမေးလ်တိုးချဲ့မှုအသုံးပြုသူ mail နဲ့ alias ကို Attribute တွေနှင့်အတူ directory ကိုချဲ့ထွင်။';
+$lang['mail_extension_aliases'] = 'နာမည်တုများ';
+$lang['mail_extension_email'] = 'E-mail ကို';
+$lang['mail_extension_forwarders'] = 'ရှေ့သို့';
+$lang['mail_extension_mail'] = 'စာပို့';
+$lang['mail_extension_email_invalid'] = 'e-mail လိပ်စာမမှန်ကန်ဘူး။';
+$lang['mail_extension_distribution_list'] = 'ဖြန့်ဖြူးစာရင်း';
+
+$lang['mail_extension_mail_alias_invalid'] = 'မေးလ် alias ကိုမမှန်ကန်ဘူး။';
+$lang['mail_extension_mail_forwarder_invalid'] = 'မေးလ်ပေးပို့ခဲ့သည်ဟုမမှန်ကန်ဘူး။';
+
