a402665b761bc325aa452d7a54741fecbc1c544b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:56:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'poŝto Pligrandigo';
+$lang['mail_extension_app_description'] = 'La Poŝto Pligrandigo etendas la dosierujo kun uzanto poŝto kaj alias atributoj.';
+$lang['mail_extension_aliases'] = 'alias';
+$lang['mail_extension_email'] = 'Retpoŝto';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'poŝto';
+$lang['mail_extension_email_invalid'] = 'Retadreso estas nevalida.';
+$lang['mail_extension_distribution_list'] = 'dissendo Listo';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias estas nevalida.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Poŝto forwarder estas nevalida.';
+
