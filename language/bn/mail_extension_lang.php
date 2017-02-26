a0feca7d5572b9fcca8b1e37d3004baea300374e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:55:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'মেল এক্সটেনশন';
+$lang['mail_extension_app_description'] = 'মেল এক্সটেনশন ব্যবহারকারী মেইল ​​ওরফে বৈশিষ্ট্যাবলী সঙ্গে ডিরেক্টরি প্রসারিত.';
+$lang['mail_extension_aliases'] = 'ওরফে';
+$lang['mail_extension_email'] = 'ই-মেইল';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'মেল';
+$lang['mail_extension_email_invalid'] = 'ই-মেইল ঠিকানা সঠিক নয়.';
+$lang['mail_extension_distribution_list'] = 'বিতরণ তালিকা';
+
+$lang['mail_extension_mail_alias_invalid'] = 'মেল ওরফে অবৈধ.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'মেল ফরওয়ার্ডিং অবৈধ.';
+
