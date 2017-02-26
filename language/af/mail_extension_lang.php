87d181695ac11bdc6099888819f4bae6b4d3bf63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:54:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Die Mail Extension strek die gids met die gebruiker pos en alias eienskappe.';
+$lang['mail_extension_aliases'] = 'aliasse';
+$lang['mail_extension_email'] = 'E-pos';
+$lang['mail_extension_forwarders'] = 'expediteurs';
+$lang['mail_extension_mail'] = 'pos';
+$lang['mail_extension_email_invalid'] = 'E-pos adres is ongeldig.';
+$lang['mail_extension_distribution_list'] = 'Verspreidingslys';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Pos alias is ongeldig.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Pos expediteur is ongeldig.';
+
