e6b0621fcc7998b9d276d06adf2e4db00b673b68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:54:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'امتداد بريد إلكتروني';
+$lang['mail_extension_app_description'] = 'وملحق بريد يمتد الدليل مع بريد المستخدم والاسم المستعار الصفات.';
+$lang['mail_extension_aliases'] = 'اسماء مستعارة';
+$lang['mail_extension_email'] = 'البريد الالكتروني';
+$lang['mail_extension_forwarders'] = 'الناقلون';
+$lang['mail_extension_mail'] = 'بريد';
+$lang['mail_extension_email_invalid'] = 'عنوان البريد الإلكتروني غير صالح.';
+$lang['mail_extension_distribution_list'] = 'قائمة التوزيع';
+
+$lang['mail_extension_mail_alias_invalid'] = 'الاسم المستعار الإلكتروني غير صالح.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'وكيل شحن البريد غير صالح.';
+
