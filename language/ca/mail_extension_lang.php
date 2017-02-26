62247b093cc0d471a1fe0ed47a9f7585e4c7de98
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:55:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'El Mail Extension estén directori amb atributs de correu d&#39;usuari o àlies.';
+$lang['mail_extension_aliases'] = 'àlies';
+$lang['mail_extension_email'] = 'Correu electrònic';
+$lang['mail_extension_forwarders'] = 'transitaris';
+$lang['mail_extension_mail'] = 'correu';
+$lang['mail_extension_email_invalid'] = 'Adreça de correu electrònic no és vàlida.';
+$lang['mail_extension_distribution_list'] = 'Llista de distribució';
+
+$lang['mail_extension_mail_alias_invalid'] = 'àlies de correu electrònic no és vàlida.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'emissor de correu no és vàlid.';
+
