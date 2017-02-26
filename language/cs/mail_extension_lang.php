99568c6460d2d230e852459e8cb9ed905eaf81b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 13:56:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension rozšiřuje adresář s uživateli pošty a alias atributy.';
+$lang['mail_extension_aliases'] = 'Přezdívky';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Transportéry';
+$lang['mail_extension_mail'] = 'Pošta';
+$lang['mail_extension_email_invalid'] = 'E-mailová adresa je neplatná.';
+$lang['mail_extension_distribution_list'] = 'Distribuční seznam';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mailový alias je neplatný.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail Dopravce je neplatný.';
+
