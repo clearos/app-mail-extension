f2bfb74a6ad073f51e550daabf7157074a094a5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:33:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = '메일 확장';
+$lang['mail_extension_app_description'] = '메일 확장 사용자 메일 및 별칭 속성이있는 디렉토리를 확장합니다.';
+$lang['mail_extension_aliases'] = '별칭';
+$lang['mail_extension_email'] = 'E 메일';
+$lang['mail_extension_forwarders'] = '포워더';
+$lang['mail_extension_mail'] = '우편';
+$lang['mail_extension_email_invalid'] = 'E 메일 주소가 잘못되었습니다.';
+$lang['mail_extension_distribution_list'] = '배포 목록';
+
+$lang['mail_extension_mail_alias_invalid'] = '메일 별칭이 잘못되었습니다.';
+$lang['mail_extension_mail_forwarder_invalid'] = '메일 전달자가 잘못되었습니다.';
+
