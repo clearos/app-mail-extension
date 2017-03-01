9685562b385b3e9f5f91c448f590d119bf1315ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:57:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Тамдиди';
+$lang['mail_extension_app_description'] = 'The Mail Тамдиди тамдид феҳрист бо почтаи корбар ва номи кӯтоҳшуда хусусиятҳои.';
+$lang['mail_extension_aliases'] = 'сарҳарфи';
+$lang['mail_extension_email'] = 'E-почтаи электронӣ';
+$lang['mail_extension_forwarders'] = 'ирсолкунандагон';
+$lang['mail_extension_mail'] = 'почта';
+$lang['mail_extension_email_invalid'] = 'Нишонаи электронии нодуруст аст.';
+$lang['mail_extension_distribution_list'] = 'Рӯйхати Тақсими';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail номи кӯтоҳшуда нодуруст аст.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail экспедитор нодуруст аст.';
+
