991958c57a424645ce8e065322660cdd701b971c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:30:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'ფოსტა გაფართოება';
+$lang['mail_extension_app_description'] = 'ფოსტა გაფართოება ვრცელდება დირექტორია შესახებ mail და იგივე ატრიბუტები.';
+$lang['mail_extension_aliases'] = 'aliases';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'ექსპედიტორთა';
+$lang['mail_extension_mail'] = 'ფოსტა';
+$lang['mail_extension_email_invalid'] = 'E-mail მისამართი არასწორია.';
+$lang['mail_extension_distribution_list'] = 'განაწილების სია';
+
+$lang['mail_extension_mail_alias_invalid'] = 'ფოსტა alias არასწორია.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'ფოსტა ექსპედიტორი არასწორია.';
+
