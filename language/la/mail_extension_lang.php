917ea7ff47b95a33f5a79b44a69ec51721980acd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:34:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail extensio';
+$lang['mail_extension_app_description'] = 'Mail est extensio usque in user cum Directory Email et alias passiones.';
+$lang['mail_extension_aliases'] = 'aliases';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'E-mail nulla.';
+$lang['mail_extension_distribution_list'] = 'Distribution List';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias irrita est.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail Forwarder est invalidum.';
+
