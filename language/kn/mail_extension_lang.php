88e762a57d1e20d0da2843b5dca8a86001c3bc2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:33:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'ಮೇಲ್ ವಿಸ್ತರಣೆ';
+$lang['mail_extension_app_description'] = 'ಮೇಲ್ ವಿಸ್ತರಣೆ ಬಳಕೆದಾರ ಮೇಲ್ ಮತ್ತು ಅಲಿಯಾಸ್ ಲಕ್ಷಣಗಳು ಕೋಶವನ್ನು ವಿಸ್ತರಿಸುತ್ತದೆ.';
+$lang['mail_extension_aliases'] = 'ಅಲಿಯಾಸ್';
+$lang['mail_extension_email'] = 'ಮೇಲ್';
+$lang['mail_extension_forwarders'] = 'ರವಾನಿಸುವವರನ್ನು';
+$lang['mail_extension_mail'] = 'ಮೇಲ್';
+$lang['mail_extension_email_invalid'] = 'ಇ ಮೇಲ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mail_extension_distribution_list'] = 'ವಿತರಣಾ ಪಟ್ಟಿ';
+
+$lang['mail_extension_mail_alias_invalid'] = 'ಮೇಲ್ ಅಲಿಯಾಸ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'ಮೇಲ್ ರವಾನಿಸುವವರು ಅಮಾನ್ಯವಾಗಿದೆ.';
+
