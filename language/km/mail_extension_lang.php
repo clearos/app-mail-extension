6d5c284e9bb7e0e3fa683f3697d6a314200027f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:32:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'ផ្នែកបន្ថែមសំបុត្រ';
+$lang['mail_extension_app_description'] = 'ផ្នែកបន្ថែមសំបុត្រពង្រីកថតដែលមានគុណលក្ខណៈសំបុត្រអ្នកប្រើនិងឈ្មោះក្លែងក្លាយ។';
+$lang['mail_extension_aliases'] = 'ឈ្មោះក្លែងក្លាយ';
+$lang['mail_extension_email'] = 'អ៊ីម៉ែល';
+$lang['mail_extension_forwarders'] = 'បញ្ជូន';
+$lang['mail_extension_mail'] = 'សំបុត្រ';
+$lang['mail_extension_email_invalid'] = 'អាសយដ្ឋានអ៊ីមែលមិនត្រឹមត្រូវ។';
+$lang['mail_extension_distribution_list'] = 'បញ្ជី​ចែកចាយ';
+
+$lang['mail_extension_mail_alias_invalid'] = 'ឈ្មោះក្លែងក្លាយសំបុត្រមិនត្រឹមត្រូវ។';
+$lang['mail_extension_mail_forwarder_invalid'] = 'បញ្ជូនបន្តសំបុត្រមិនត្រឹមត្រូវ។';
+
