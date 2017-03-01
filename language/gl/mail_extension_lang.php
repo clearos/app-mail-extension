c732e42fb775740bd511d5110d856079b01b4482
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:20:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'e Extension';
+$lang['mail_extension_app_description'] = 'O ramal de correo estende o directorio con atributos de correo do usuario e alias.';
+$lang['mail_extension_aliases'] = 'alias';
+$lang['mail_extension_email'] = 'Correo-e';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'correo';
+$lang['mail_extension_email_invalid'] = 'Enderezo de correo electrónico non é válido.';
+$lang['mail_extension_distribution_list'] = 'Lista de distribución';
+
+$lang['mail_extension_mail_alias_invalid'] = 'alias de correo electrónico non é válido.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'TRANSITARIOS electrónico non é válido.';
+
