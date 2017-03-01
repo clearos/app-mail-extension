414c302704ebc88cad9b743f348b855b6adea93f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:49:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'තැපැල් ව්යාප්ති';
+$lang['mail_extension_app_description'] = 'ඇති තැපැල් ව්යාප්ති පරිශීලක තැපැල් සහ හෙවත් ලක්ෂණ සහිත බහලුම ව්යාප්ත කරයි.';
+$lang['mail_extension_aliases'] = 'යළි-යොමුවීම්';
+$lang['mail_extension_email'] = 'ඊ-තැපැල';
+$lang['mail_extension_forwarders'] = 'ගෙන යන්නන්';
+$lang['mail_extension_mail'] = 'තැපැල්';
+$lang['mail_extension_email_invalid'] = 'ඊ-තැපැල් ලිපිනය වලංගු නැත.';
+$lang['mail_extension_distribution_list'] = 'බෙදා හැරීම ලැයිස්තුව';
+
+$lang['mail_extension_mail_alias_invalid'] = 'තැපැල් හෙවත් වලංගු නැත.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'තැපැල් forwarder වලංගු නැත.';
+
