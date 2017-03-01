d54adae1e4580b2ef83004127924a6013273bee8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:56:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'మెయిల్ పొడిగింపు';
+$lang['mail_extension_app_description'] = 'మెయిల్ వినియోగదారు పొడిగింపు మెయిల్ అలియాస్ గుణ డైరెక్టరీ విస్తరించింది.';
+$lang['mail_extension_aliases'] = 'మారుపేర్ల';
+$lang['mail_extension_email'] = 'ఇ-మెయిల్';
+$lang['mail_extension_forwarders'] = 'ఫార్వర్డర్లను';
+$lang['mail_extension_mail'] = 'మెయిల్';
+$lang['mail_extension_email_invalid'] = 'ఇ మె యి ఎల్ చిరునామ సరియనది కాదు.';
+$lang['mail_extension_distribution_list'] = 'పంపిణీ జాబితా';
+
+$lang['mail_extension_mail_alias_invalid'] = 'మెయిల్ అలియాస్ చెల్లదు.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'మెయిల్ ఫార్వర్డర్ చెల్లదు.';
+
