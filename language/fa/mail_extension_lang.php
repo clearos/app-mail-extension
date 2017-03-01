2f79ea4e97653ef2ed00b17232d6532f4cb3958a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:45:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'ایمیل فرمت';
+$lang['mail_extension_app_description'] = 'ایمیل فرمت گسترش دایرکتوری با ایمیل کاربران و نام مستعار ویژگی ها است.';
+$lang['mail_extension_aliases'] = 'نام مستعار';
+$lang['mail_extension_email'] = 'فرستادن به ایمیل';
+$lang['mail_extension_forwarders'] = 'حمل و نقل';
+$lang['mail_extension_mail'] = 'پست';
+$lang['mail_extension_email_invalid'] = 'آدرس ایمیل معتبر نیست.';
+$lang['mail_extension_distribution_list'] = 'لیست توزیع';
+
+$lang['mail_extension_mail_alias_invalid'] = 'ایمیل مستعار نامعتبر است.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'ایمیل فورواردر نامعتبر است.';
+
