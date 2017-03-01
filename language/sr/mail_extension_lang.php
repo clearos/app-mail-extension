007dd235d41fb5cd603536812f402a3edd44e323
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:49:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'маил Проширење';
+$lang['mail_extension_app_description'] = 'Тхе Маил Ектенсион проширује директоријум са корисника поште и алиас атрибута.';
+$lang['mail_extension_aliases'] = 'алиасес';
+$lang['mail_extension_email'] = 'Е-пошта';
+$lang['mail_extension_forwarders'] = 'Транспортери';
+$lang['mail_extension_mail'] = 'Пошта';
+$lang['mail_extension_email_invalid'] = 'Е-маил адреса није исправна.';
+$lang['mail_extension_distribution_list'] = 'Листа дистрибуција';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Маил псеудоним је неважећи.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Маил шпедитера је неважећи.';
+
