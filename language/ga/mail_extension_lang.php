4bacc24e3a278ded0d85d43cc61cd6465ba493f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:27:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Síneadh Mail';
+$lang['mail_extension_app_description'] = 'An Leathnú Mail Síneann an t-eolaire a bhfuil post úsáideora agus ailias tréithe.';
+$lang['mail_extension_aliases'] = 'ailiasanna';
+$lang['mail_extension_email'] = 'Ríomhphost';
+$lang['mail_extension_forwarders'] = 'seoltóirí';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'Is Seoladh r-phoist neamhbhailí.';
+$lang['mail_extension_distribution_list'] = 'Liosta dáilte';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Is Mail ailias neamhbhailí.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Is Mail seoltóir neamhbhailí.';
+
