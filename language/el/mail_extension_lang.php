bf58d9a2ad264072633580a19bb5261fca1a7885
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:21:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Επέκταση Mail';
+$lang['mail_extension_app_description'] = 'Η επέκταση Mail επεκτείνει τον κατάλογο με ταχυδρομείο χρήστη και το ψευδώνυμο χαρακτηριστικά.';
+$lang['mail_extension_aliases'] = 'ψευδώνυμα';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Μεταφορείς';
+$lang['mail_extension_mail'] = 'Ταχυδρομείο';
+$lang['mail_extension_email_invalid'] = 'Η διεύθυνση ηλεκτρονικού ταχυδρομείου δεν είναι έγκυρη.';
+$lang['mail_extension_distribution_list'] = 'Λίστα διανομής';
+
+$lang['mail_extension_mail_alias_invalid'] = 'ψευδώνυμο ταχυδρομείου δεν είναι έγκυρη.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'πράκτορας ταχυδρομείου δεν είναι έγκυρη.';
+
