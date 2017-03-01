d1b37cb80d157c0e6ed9040805c49493359cc8db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:55:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension utökar katalog med användar post och aliasattribut.';
+$lang['mail_extension_aliases'] = 'alias';
+$lang['mail_extension_email'] = 'E-post';
+$lang['mail_extension_forwarders'] = 'skot';
+$lang['mail_extension_mail'] = 'Post';
+$lang['mail_extension_email_invalid'] = 'Emailadressen är ogiltig.';
+$lang['mail_extension_distribution_list'] = 'Distributionslista';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Postalias är ogiltig.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail skotare är ogiltig.';
+
