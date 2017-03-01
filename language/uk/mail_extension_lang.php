2f529a7018e81c86d62fcdfc8106b32edfa229cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:59:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension розширює каталог з користувачем пошти та псевдонімів атрибутів.';
+$lang['mail_extension_aliases'] = 'псевдоніми';
+$lang['mail_extension_email'] = 'Електронна пошта';
+$lang['mail_extension_forwarders'] = 'експедитори';
+$lang['mail_extension_mail'] = 'пошта';
+$lang['mail_extension_email_invalid'] = 'Адреса електронної пошти є недійсним.';
+$lang['mail_extension_distribution_list'] = 'рознарядка';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Поштовий псевдонім є недійсним.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Пересилання пошти є недійсним.';
+
