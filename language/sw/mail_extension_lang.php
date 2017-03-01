67a6c8bba54267cd2f5c66cb8825ff765d79f6e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:54:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Barua ugani';
+$lang['mail_extension_app_description'] = 'Barua ugani hadi directory na sifa user pepe na pak.';
+$lang['mail_extension_aliases'] = 'Majina bandia';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Barua';
+$lang['mail_extension_email_invalid'] = 'E-pepe ni batili.';
+$lang['mail_extension_distribution_list'] = 'Distribution Orodha';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Barua alias ni batili.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Barua forwarder ni batili.';
+
