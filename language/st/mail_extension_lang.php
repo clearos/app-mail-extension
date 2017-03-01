0dbe147dc43e89ce2ef8740e794a4413a5af749d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:52:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension fetela ea dibaka le litšobotsi user want-meet.ru mail le alias.';
+$lang['mail_extension_aliases'] = 'mabitso a boikhakanyo';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'Aterese ya imeile ha o sebetse.';
+$lang['mail_extension_distribution_list'] = 'kabo List';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias ha o sebetse.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder ha o sebetse.';
+
