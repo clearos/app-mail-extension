67cd15d4ef9341c1678c97bb3bea98e37741252f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:23:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Ekstansyon';
+$lang['mail_extension_app_description'] = 'Ekstansyon an Mail fin anyè a ak lapòs itilizatè yo ak jaden atribi.';
+$lang['mail_extension_aliases'] = 'anpren';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'komisionèr';
+$lang['mail_extension_mail'] = 'mail';
+$lang['mail_extension_email_invalid'] = 'E-mail adrès se valab.';
+$lang['mail_extension_distribution_list'] = 'Distribisyon Lis';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail jaden sa se valab.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail komisèr se valab.';
+
