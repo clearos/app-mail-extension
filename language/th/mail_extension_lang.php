e500f202cf5a315069c20a4565fc724252edca38
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:57:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'จดหมายนามสกุล';
+$lang['mail_extension_app_description'] = 'ส่วนต่อขยายของจดหมายขยายไดเรกทอรีที่มีคุณลักษณะที่อีเมลของผู้ใช้และนามแฝง';
+$lang['mail_extension_aliases'] = 'นามแฝง';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'ลำเลียง';
+$lang['mail_extension_mail'] = 'จดหมาย';
+$lang['mail_extension_email_invalid'] = 'E-mail address ไม่ถูกต้อง';
+$lang['mail_extension_distribution_list'] = 'รายชื่อการแจกจ่าย';
+
+$lang['mail_extension_mail_alias_invalid'] = 'จดหมายนามแฝงไม่ถูกต้อง';
+$lang['mail_extension_mail_forwarder_invalid'] = 'จดหมายส่งไม่ถูกต้อง';
+
