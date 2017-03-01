d1d6b8ce1d0861b9dff548ccad1ce1f858dcd583
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:24:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Tsawo';
+$lang['mail_extension_app_description'] = 'The Mail Tsawo kara shugabanci da mai amfani mail da kuma wanda aka ce masa halaye.';
+$lang['mail_extension_aliases'] = 'laƙabi';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'E-mail address ba daidai ba ne.';
+$lang['mail_extension_distribution_list'] = 'rarraba List';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail wanda aka ce masa ba daidai ba ne.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder ba daidai ba ne.';
+
