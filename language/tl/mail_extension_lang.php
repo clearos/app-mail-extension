f098eee5482c64db34b62681488427a18c0f9299
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:58:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Extension mail';
+$lang['mail_extension_app_description'] = 'Ang Mail Extension nagpalawak sa direktoryo na may user mail at alias katangian.';
+$lang['mail_extension_aliases'] = 'alias';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'koreo';
+$lang['mail_extension_email_invalid'] = 'E-mail address ay hindi wasto.';
+$lang['mail_extension_distribution_list'] = 'Listahan distribution';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias ay hindi wasto.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail tagapagpatuloy ay hindi wasto.';
+
