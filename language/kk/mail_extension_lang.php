c890886704a87f675bc350c655684a0c973be43b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:31:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Пошта ұзарту пайдаланушы пошта және бүркеншік атрибуттары бар каталогты кеңейтеді.';
+$lang['mail_extension_aliases'] = 'Бүркеншік';
+$lang['mail_extension_email'] = 'Электрондық пошта';
+$lang['mail_extension_forwarders'] = 'экспедиторлар';
+$lang['mail_extension_mail'] = 'почта';
+$lang['mail_extension_email_invalid'] = 'Электрондық пошта мекенжайы жарамсыз болып табылады.';
+$lang['mail_extension_distribution_list'] = 'тарату тізімі';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Пошта бүркеншік атын жарамсыз болып табылады.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Пошта экспедитор жарамсыз болып табылады.';
+
