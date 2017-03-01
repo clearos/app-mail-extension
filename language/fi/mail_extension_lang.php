3d5d828e0e037b3289a8f33424e6eba98f476bd4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:18:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension ulottuu hakemistoon käyttäjä postin ja alias määritteitä.';
+$lang['mail_extension_aliases'] = 'aliakset';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Kuormatraktorit';
+$lang['mail_extension_mail'] = 'posti';
+$lang['mail_extension_email_invalid'] = 'Sähköpostiosoite on virheellinen.';
+$lang['mail_extension_distribution_list'] = 'Jakelulista';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias on virheellinen.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail kuormatraktori on virheellinen.';
+
