7d42fb3f1f74fc4c411fd1b15e4b26d396972b4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:28:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension framlengir skrá með notanda póstur og alias eiginleika.';
+$lang['mail_extension_aliases'] = 'samheiti';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'mail';
+$lang['mail_extension_email_invalid'] = 'E-mail er ógild.';
+$lang['mail_extension_distribution_list'] = 'Dreifing List';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias er ógilt.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail framsendingar er ógilt.';
+
