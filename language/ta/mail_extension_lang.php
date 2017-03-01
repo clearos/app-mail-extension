b9b8fed7247b4640e0345fb389b9299c73201f60
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:55:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'மின்னஞ்சல் நீட்டிப்பு';
+$lang['mail_extension_app_description'] = 'மின்னஞ்சல் நீட்டிப்பு பயனர் மெயில் மறுபெயரைப் பண்புகளை கொண்ட அடைவு பரவியுள்ளது.';
+$lang['mail_extension_aliases'] = 'மாற்றுப்பெயர்கள்';
+$lang['mail_extension_email'] = 'மின்னஞ்சல்';
+$lang['mail_extension_forwarders'] = 'போக்குவரத்தாளர்களின்';
+$lang['mail_extension_mail'] = 'மின்னஞ்சல்';
+$lang['mail_extension_email_invalid'] = 'E-mail முகவரி தவறானது.';
+$lang['mail_extension_distribution_list'] = 'விநியோக பட்டியல்';
+
+$lang['mail_extension_mail_alias_invalid'] = 'மின்னஞ்சல் மறுபெயர் தவறானது.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'மின்னஞ்சல் அனுப்புவர் தவறானது.';
+
