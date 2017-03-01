9dd4df9b7195f6eccce33b1f537c184cedea6d0b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:43:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'मेल विस्तार';
+$lang['mail_extension_app_description'] = 'मेल विस्तार प्रयोगकर्ता मेल र उपनाम विशेषताहरु संग निर्देशिका विस्तार गर्दछ।';
+$lang['mail_extension_aliases'] = 'उपनाम';
+$lang['mail_extension_email'] = 'ई-मेल';
+$lang['mail_extension_forwarders'] = 'अग्रेषकों';
+$lang['mail_extension_mail'] = 'मेल';
+$lang['mail_extension_email_invalid'] = 'ई-मेल ठेगाना अवैध छ।';
+$lang['mail_extension_distribution_list'] = 'वितरण सूची';
+
+$lang['mail_extension_mail_alias_invalid'] = 'मेल उपनाम अमान्य छ।';
+$lang['mail_extension_mail_forwarder_invalid'] = 'मेल forwarder अमान्य छ।';
+
