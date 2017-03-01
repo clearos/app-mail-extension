a8ef0cd977e2e30e6b07471559375f0f5c872c3b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:30:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'メール拡張';
+$lang['mail_extension_app_description'] = 'メール拡張は、ユーザーのメールとエイリアス属性を持つディレクトリを拡張します。';
+$lang['mail_extension_aliases'] = 'エイリアス';
+$lang['mail_extension_email'] = 'Eメール';
+$lang['mail_extension_forwarders'] = 'フォワーダ';
+$lang['mail_extension_mail'] = '郵便物';
+$lang['mail_extension_email_invalid'] = 'Eメールアドレスが無効です。';
+$lang['mail_extension_distribution_list'] = '配布リスト';
+
+$lang['mail_extension_mail_alias_invalid'] = 'メールエイリアスが無効です。';
+$lang['mail_extension_mail_forwarder_invalid'] = 'メールフォワーダが無効です。';
+
