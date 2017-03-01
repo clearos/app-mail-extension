1e8e9c649904b29a598073524439bb27c1344089
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:39:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'മെയിൽ വിപുലീകരണം';
+$lang['mail_extension_app_description'] = 'മെയിൽ വിപുലീകരണ ഉപയോക്താവ് മെയിൽ, അപരനാമം ആട്രിബ്യൂട്ടുകളുമുള്ള ഡയറക്ടറി വ്യാപിച്ചിരിക്കുന്നു.';
+$lang['mail_extension_aliases'] = 'ഇരട്ടപ്പേരുകൾ';
+$lang['mail_extension_email'] = 'ഇ-മെയിൽ';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'മെയിൽ';
+$lang['mail_extension_email_invalid'] = 'ഇ - മെയിൽ വിലാസം അസാധുവാണ്.';
+$lang['mail_extension_distribution_list'] = 'വിതരണ പട്ടിക';
+
+$lang['mail_extension_mail_alias_invalid'] = 'മെയിൽ അപരനാമം അസാധുവാണ്.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'മെയിൽ ഫോര്വേഡാര് അസാധുവാണ്.';
+
