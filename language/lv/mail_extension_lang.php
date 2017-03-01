014fc2a2ea321a9da4340a37aae728f277cae454
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:36:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'pasts Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension paplašina direktoriju ar lietotāju pasta un alias atribūtiem.';
+$lang['mail_extension_aliases'] = 'pseidonīmi';
+$lang['mail_extension_email'] = 'E-pasts';
+$lang['mail_extension_forwarders'] = 'Forvarderi';
+$lang['mail_extension_mail'] = 'pasts';
+$lang['mail_extension_email_invalid'] = 'E-pasta adrese nav derīga.';
+$lang['mail_extension_distribution_list'] = 'Distribution saraksts';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Pasts alias ir nederīgs.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Pasts ekspeditors ir nederīgs.';
+
