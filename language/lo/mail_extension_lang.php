9d6acb7cb91ab3f8a0e5e60d673cc711aa479608
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:35:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'ການຂະຫຍາຍ mail';
+$lang['mail_extension_app_description'] = 'ການຂະຫຍາຍ Mail ຂະຫຍາຍລະບົບທີ່ມີຄຸນລັກສະນະສະມາຊິກເມລແລະ alias.';
+$lang['mail_extension_aliases'] = 'ຊື່ແທນ';
+$lang['mail_extension_email'] = 'ອີເມລ໌';
+$lang['mail_extension_forwarders'] = 'ລົດລໍາລຽງ';
+$lang['mail_extension_mail'] = 'mail';
+$lang['mail_extension_email_invalid'] = 'ທີ່ຢູ່ອີເມລ໌ບໍ່ຖືກຕ້ອງ.';
+$lang['mail_extension_distribution_list'] = 'ບັນຊີການແຜ່ກະຈາຍ';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail Forward ແມ່ນບໍ່ຖືກຕ້ອງ.';
+
