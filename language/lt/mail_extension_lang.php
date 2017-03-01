d2aa46d274b859e43acdb0241d46f23e0c3ddb40
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:35:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'paštas pratęsimas';
+$lang['mail_extension_app_description'] = 'Paštas pratęsimas tęsiasi katalogą su vartotojo laiškus ir pravardes atributus.';
+$lang['mail_extension_aliases'] = 'slapyvardžiai';
+$lang['mail_extension_email'] = 'Elektroninis paštas';
+$lang['mail_extension_forwarders'] = 'miškovežiai';
+$lang['mail_extension_mail'] = 'paštas';
+$lang['mail_extension_email_invalid'] = 'Pašto adresas yra neteisingas.';
+$lang['mail_extension_distribution_list'] = 'platinimo sąrašas';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Paštas slapyvardis yra neteisinga.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Pašto ekspeditorius yra neteisinga.';
+
