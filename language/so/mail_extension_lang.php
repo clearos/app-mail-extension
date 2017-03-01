02eb3dca028d73d7e6a501cf00672fae2471f81b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:51:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension The kordhiyay buugga la user mail iyo alias sifooyinka.';
+$lang['mail_extension_aliases'] = 'magacyadooda';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'Cinwaanka E-mail waa baadil.';
+$lang['mail_extension_distribution_list'] = 'Liiska Qaybinta';
+
+$lang['mail_extension_mail_alias_invalid'] = 'alias Mail waa baadil.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'forwarder Mail waa baadil.';
+
