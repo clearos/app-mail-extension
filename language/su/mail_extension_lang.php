91769ce0dc1daa1cf95da1f819bfb44d7b893ceb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:53:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'surat penyuluh';
+$lang['mail_extension_app_description'] = 'Cijeungjing penyuluh ngalegaan nu diréktori jeung mail pamaké jeung alias atribut.';
+$lang['mail_extension_aliases'] = 'Aliases';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'pos';
+$lang['mail_extension_email_invalid'] = 'alamat surélék anu sah.';
+$lang['mail_extension_distribution_list'] = 'Daptar sebaran';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Surat alias geus sah.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Surat forwarder téh sah.';
+
