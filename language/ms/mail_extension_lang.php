db9464be08448bb36a8451b29b138d2969dcb04d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:41:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension memanjangkan direktori dengan mel pengguna dan alias sifat-sifat.';
+$lang['mail_extension_aliases'] = 'alias';
+$lang['mail_extension_email'] = 'E-mel';
+$lang['mail_extension_forwarders'] = 'Penghantar depan';
+$lang['mail_extension_mail'] = 'mail';
+$lang['mail_extension_email_invalid'] = 'Alamat e-mel tidak sah.';
+$lang['mail_extension_distribution_list'] = 'Senarai Edaran';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias tidak sah.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder tidak sah.';
+
