d32294b98214036c5cb79f06a1b30b92bf1a8a09
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:38:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Toronga';
+$lang['mail_extension_app_description'] = 'Te Mail Extension atu te whaiaronga ki mēra kaiwhakamahi, me te ingoakē huanga.';
+$lang['mail_extension_aliases'] = 'ingoakē';
+$lang['mail_extension_email'] = 'E-mēra';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'He muhu wāhitau E-mēra.';
+$lang['mail_extension_distribution_list'] = 'Rārangi Tūaritanga';
+
+$lang['mail_extension_mail_alias_invalid'] = 'He muhu ingoakē mēra.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'He muhu forwarder mēra.';
+
