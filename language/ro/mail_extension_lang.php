219b937b42c8fdb85f8d4305714872eb8364df6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:47:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail prin Extensie';
+$lang['mail_extension_app_description'] = 'Extensia Mail extinde directorul cu atributele de mail de utilizator și alias.';
+$lang['mail_extension_aliases'] = 'aliasurile';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Transportoare';
+$lang['mail_extension_mail'] = 'Poștă';
+$lang['mail_extension_email_invalid'] = 'Adresa de email este invalida.';
+$lang['mail_extension_distribution_list'] = 'Lista de distributie';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail prin alias este nevalid.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'E-mail expeditor este nevalid.';
+
