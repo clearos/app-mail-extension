d639bca98a4232dd993667faba8646e1669a7db5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:29:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'L&#39;estensione di posta estende la directory con posta elettronica degli utenti e alias attributi.';
+$lang['mail_extension_aliases'] = 'Alias';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Spedizionieri';
+$lang['mail_extension_mail'] = 'posta';
+$lang['mail_extension_email_invalid'] = 'L&#39;indirizzo email non è valido.';
+$lang['mail_extension_distribution_list'] = 'Lista di distribuzione';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias non è valido.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mittente di Posta non è valido.';
+
