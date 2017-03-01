2c7dbbf64398b65b9404daec9dc7fe9cbae52050
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:45:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'ਮੇਲ ਐਕਸ਼ਟੇਸ਼ਨ';
+$lang['mail_extension_app_description'] = 'ਮੇਲ ਐਕਸ਼ਟੇਸ਼ਨ ਉਪਭੋਗੀ ਨੂੰ ਮੇਲ ਅਤੇ ਉਰਫ ਗੁਣ ਨਾਲ ਡਾਇਰੈਕਟਰੀ ਨੂੰ ਕਵਰ ਕਰਦਾ ਹੈ.';
+$lang['mail_extension_aliases'] = 'ਉਪਨਾਮ';
+$lang['mail_extension_email'] = 'ਈ-ਮੇਲ';
+$lang['mail_extension_forwarders'] = 'ਫਾਰਵਰਡਰਜ਼';
+$lang['mail_extension_mail'] = 'ਮੇਲ';
+$lang['mail_extension_email_invalid'] = 'ਈ-ਮੇਲ ਪਤਾ ਗਲਤ ਹੈ.';
+$lang['mail_extension_distribution_list'] = 'ਵੰਡ ਸੂਚੀ';
+
+$lang['mail_extension_mail_alias_invalid'] = 'ਮੇਲ ਉਰਫ ਗਲਤ ਹੈ.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'ਮੇਲ ਫਾਰਵਰਡਰ ਗਲਤ ਹੈ.';
+
