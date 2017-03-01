28dbfc03d1bdae546b68690c39120a91dc2db82b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:42:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'posta Estensjoni';
+$lang['mail_extension_app_description'] = 'Il Estensjoni Mail testendi l-direttorju ma &#39;attributi posta utent u psewdonimi.';
+$lang['mail_extension_aliases'] = 'psewdonimi';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'posta';
+$lang['mail_extension_email_invalid'] = 'E-mail huwa invalidu.';
+$lang['mail_extension_distribution_list'] = 'Lista distribuzzjoni';
+
+$lang['mail_extension_mail_alias_invalid'] = 'alias tal-posta huwa invalidu.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'speditur mail huwa invalidu.';
+
