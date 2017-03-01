a67cf6cf653f5d461aa88d62929000c957e5d252
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:40:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Захидал Өргөтгөл';
+$lang['mail_extension_app_description'] = 'Захидал Өргөтгөл хэрэглэгч мэйл болон хаягийн шинж санг өргөтгөн.';
+$lang['mail_extension_aliases'] = 'холбоосууд';
+$lang['mail_extension_email'] = 'И-мэйл';
+$lang['mail_extension_forwarders'] = 'илгээгч';
+$lang['mail_extension_mail'] = 'Захидал';
+$lang['mail_extension_email_invalid'] = 'И-мэйл хаяг буруу байна.';
+$lang['mail_extension_distribution_list'] = 'Түгээх жагсаалт';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Мэйл хаягийн хүчин төгөлдөр бус байна.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail зуучлал хүчин төгөлдөр бус байна.';
+
