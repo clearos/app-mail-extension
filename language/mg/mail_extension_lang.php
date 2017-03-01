93f442a1995cbde7c9603db8fc5c6e149e192804
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:37:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'Ny Mail Extension dia nanitatra ny lahatahiry amin&#39;ny mpampiasa sy ny antsoina namany Sary toetra.';
+$lang['mail_extension_aliases'] = 'Niovaova anarana';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'E-mail dia tsy manan-kery.';
+$lang['mail_extension_distribution_list'] = 'Distribution List';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail antsoina dia tsy manan-kery.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder dia tsy manan-kery.';
+
