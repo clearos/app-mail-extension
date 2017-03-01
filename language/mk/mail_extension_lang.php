cb2abc20dfa09b7477084e368f8c19917a7c6eab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:38:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension протега директориумот со корисник mail и алијас атрибути.';
+$lang['mail_extension_aliases'] = 'Алијаси';
+$lang['mail_extension_email'] = 'Е-пошта';
+$lang['mail_extension_forwarders'] = 'шпедитери';
+$lang['mail_extension_mail'] = 'пошта';
+$lang['mail_extension_email_invalid'] = 'E-mail адреса не е валидна.';
+$lang['mail_extension_distribution_list'] = 'дистрибуција Листа';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Пошта алијас не е валиден.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Пошта шпедитерот е валиден.';
+
