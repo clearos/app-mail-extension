656936fb965fd7fe4539aaea1c66332a0973136a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:27:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension gbatịrị na ndekọ na onye ọrụ mail na utu aha àgwà.';
+$lang['mail_extension_aliases'] = 'Aliases';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'mail';
+$lang['mail_extension_email_invalid'] = 'E-mail address bụ ghara ịdị irè.';
+$lang['mail_extension_distribution_list'] = 'nkesa List';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail utu aha bụ ghara ịdị irè.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder bụ ghara ịdị irè.';
+
