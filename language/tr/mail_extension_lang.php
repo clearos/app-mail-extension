24c0c700ec1710bee105607d035f6324f3481340
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:59:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'posta Uzantısı';
+$lang['mail_extension_app_description'] = 'Posta Uzantısı kullanıcı posta ve diğer adı nitelikleri ile dizin uzanır.';
+$lang['mail_extension_aliases'] = 'Diğer adlar';
+$lang['mail_extension_email'] = 'E-posta';
+$lang['mail_extension_forwarders'] = 'Tomruk';
+$lang['mail_extension_mail'] = 'Posta';
+$lang['mail_extension_email_invalid'] = 'Email adresi geçersiz.';
+$lang['mail_extension_distribution_list'] = 'Dağıtım listesi';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Posta takma geçersiz.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Posta iletici geçersiz.';
+
