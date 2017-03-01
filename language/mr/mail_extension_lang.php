27bb85d2fcebc9b15719ac193fc1913ff1746ae1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:40:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'मेल विस्तार';
+$lang['mail_extension_app_description'] = 'मेल विस्तार वापरकर्ता मेल आणि ऊर्फ गुणधर्म निर्देशिकेच्या वाढवितो.';
+$lang['mail_extension_aliases'] = 'उर्फ';
+$lang['mail_extension_email'] = 'ई-मेल';
+$lang['mail_extension_forwarders'] = 'फॉरवर्डर्सकडून';
+$lang['mail_extension_mail'] = 'मेल';
+$lang['mail_extension_email_invalid'] = 'ई-मेल पत्ता अवैध आहे.';
+$lang['mail_extension_distribution_list'] = 'वितरण यादी';
+
+$lang['mail_extension_mail_alias_invalid'] = 'मेल उपाख्य अवैध आहे.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'मेल पुरस्कर्ता अवैध आहे.';
+
