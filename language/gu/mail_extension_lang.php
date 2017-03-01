5aad1c13806fcae1a5391d8de1b53b2a5a3fb595
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:22:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'મેલ એક્સ્ટેંશન';
+$lang['mail_extension_app_description'] = 'મેલ એક્સ્ટેંશન વપરાશકર્તા મેલ અને ઉર્ફે લક્ષણો સાથે ડિરેક્ટરી લંબાય છે.';
+$lang['mail_extension_aliases'] = 'ઉપનામ';
+$lang['mail_extension_email'] = 'ઇ મેલ';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'મેલ';
+$lang['mail_extension_email_invalid'] = 'ઇ મેલ સરનામું અમાન્ય છે.';
+$lang['mail_extension_distribution_list'] = 'વિતરણ સૂચિ';
+
+$lang['mail_extension_mail_alias_invalid'] = 'મેલ ઉપનામ અમાન્ય છે.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'મેઇલ forwarder અમાન્ય છે.';
+
