123881891ab84ecb305732d771753e14619894c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-01 23:25:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'A Mail Extension kiterjeszti a könyvtár felhasználói e-mail és alias attribútumokat.';
+$lang['mail_extension_aliases'] = 'Más nevek';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Kihordók';
+$lang['mail_extension_mail'] = 'Levél';
+$lang['mail_extension_email_invalid'] = 'E-mail cím érvénytelen.';
+$lang['mail_extension_distribution_list'] = 'Terjesztési lista';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias érvénytelen.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail szállítmányozó érvénytelen.';
+
