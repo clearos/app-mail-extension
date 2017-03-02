da882b4b351f64c26685cf5b552ffe3ade36154e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-02 00:00:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'Mail Extension foydalanuvchi mail va boshqacha sifatlari bilan katalogini uzaytiradi.';
+$lang['mail_extension_aliases'] = 'laqab';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Ekspeditorlik';
+$lang['mail_extension_mail'] = 'pochta';
+$lang['mail_extension_email_invalid'] = 'E-mail manzil bekor.';
+$lang['mail_extension_distribution_list'] = 'tarqatish ro&#39;yxati';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail boshqacha haqiqiy emas.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail ekspeditorlik haqiqiy emas.';
+
