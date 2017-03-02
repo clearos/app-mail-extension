8fc7150fc034d0ae30412d7b88886a5e5ca1fd36
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-02 00:02:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'מעיל עקסטענסיאָן';
+$lang['mail_extension_app_description'] = 'די פּאָסט געשפּרייט יקסטענדז די דירעקטארי מיט באַניצער פּאָסט און אַליאַס אַטראַביוץ.';
+$lang['mail_extension_aliases'] = 'ייליאַסיז';
+$lang['mail_extension_email'] = 'E- פּאָסט';
+$lang['mail_extension_forwarders'] = 'פאָרוואַרדערס';
+$lang['mail_extension_mail'] = 'פּאָסט';
+$lang['mail_extension_email_invalid'] = 'E- פּאָסט אַדרעס איז פאַרקריפּלט.';
+$lang['mail_extension_distribution_list'] = 'פאַרשפּרייטונג רשימה';
+
+$lang['mail_extension_mail_alias_invalid'] = 'מעיל אַליאַס איז פאַרקריפּלט.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'מעיל פאָרוואַרדער איז פאַרקריפּלט.';
+
