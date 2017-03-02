5fb18d245ef8d823efe5563374f971038b530776
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-02 00:03:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Itẹsiwaju';
+$lang['mail_extension_app_description'] = 'The Mail Itẹsiwaju pan ni liana pẹlu olumulo mail ati inagijẹ eroja.';
+$lang['mail_extension_aliases'] = 'aliases';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'mail';
+$lang['mail_extension_email_invalid'] = 'E-mail adirẹsi ti wa ni invalid.';
+$lang['mail_extension_distribution_list'] = 'Distribution Akojọ';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail inagijẹ ni invalid.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder ni invalid.';
+
