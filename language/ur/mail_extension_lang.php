3a6b1fbe9e37a0d39e4e92ae7862fa0648ee777c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-02 00:00:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'میل کی توسیع';
+$lang['mail_extension_app_description'] = 'میل کی توسیع صارف میل اور عرف صفات کے ساتھ ڈائرکٹری میں توسیع.';
+$lang['mail_extension_aliases'] = 'عرفیتیں';
+$lang['mail_extension_email'] = 'ای میل';
+$lang['mail_extension_forwarders'] = 'ارسال';
+$lang['mail_extension_mail'] = 'میل';
+$lang['mail_extension_email_invalid'] = 'ای میل ایڈریس درست نہیں ہے.';
+$lang['mail_extension_distribution_list'] = 'تقسیم کی فہرست';
+
+$lang['mail_extension_mail_alias_invalid'] = 'میل عرف غلط ہے.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'میل forwarder کے باطل ہے.';
+
