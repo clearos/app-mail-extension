3fa61787b65975aea921988541ab30266c210afc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-02 00:04:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Mail Extension';
+$lang['mail_extension_app_description'] = 'The Mail Extension usinika lwemibhalo mail umsebenzisi nenye yezimfanelo.';
+$lang['mail_extension_aliases'] = 'Iziteketiso';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'Ikheli le-email address is invalid.';
+$lang['mail_extension_distribution_list'] = 'Uhlu Distribution';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Mail alias ayivumelekile.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Mail forwarder ayivumelekile.';
+
