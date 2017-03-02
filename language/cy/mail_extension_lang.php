3ad8c060fd4f792a4a052627b309f4c49e109a9c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-02 00:02:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'Estyniad Mail';
+$lang['mail_extension_app_description'] = 'Mae&#39;r Estyniad Mail yn ymestyn cyfeiriadur gyda post defnyddwyr a alias priodoleddau.';
+$lang['mail_extension_aliases'] = 'enwau eraill';
+$lang['mail_extension_email'] = 'E-bost';
+$lang['mail_extension_forwarders'] = 'Forwarders';
+$lang['mail_extension_mail'] = 'Mail';
+$lang['mail_extension_email_invalid'] = 'Cyfeiriad e-bost yn annilys.';
+$lang['mail_extension_distribution_list'] = 'Rhestr Ddosbarthu';
+
+$lang['mail_extension_mail_alias_invalid'] = 'alias bost yn annilys.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'forwarder bost yn annilys.';
+
