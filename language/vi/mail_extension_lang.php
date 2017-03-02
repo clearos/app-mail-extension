601374fe02d24b502dd78afa9fcd46a1b1ca1acc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-03-02 00:01:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/mail_extension_lang.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang['mail_extension_app_name'] = 'mail Extension';
+$lang['mail_extension_app_description'] = 'Các Mail Extension mở rộng thư mục với thư người dùng và bí danh tính.';
+$lang['mail_extension_aliases'] = 'bí danh';
+$lang['mail_extension_email'] = 'E-mail';
+$lang['mail_extension_forwarders'] = 'giao nhận';
+$lang['mail_extension_mail'] = 'Thư';
+$lang['mail_extension_email_invalid'] = 'địa chỉ E-mail không hợp lệ.';
+$lang['mail_extension_distribution_list'] = 'Danh sách phân phối';
+
+$lang['mail_extension_mail_alias_invalid'] = 'Thư bí danh là không hợp lệ.';
+$lang['mail_extension_mail_forwarder_invalid'] = 'Thư giao nhận là không hợp lệ.';
+
